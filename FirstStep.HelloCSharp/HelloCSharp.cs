0ca92b00257870801f5c5169800e9455abff467c
vesko <vesko.mitev@outlook.com>
2016-05-08 11:54:12 +0300

    Add

---
+++ FirstStep.HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstStep.HelloCSharp
+{
+using System;
+    class HelloCSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
---
+++ TriangleFrom55Stars/TriangleOfStars.cs
@@ -0,0 +1,19 @@
+﻿namespace TriangleFrom55Stars
+{
+    using System;
+    class TriangleOfStars
+    {
+        static void Main()
+        {
+            for (int row = 1; row <= 10; row++)
+            {
+                string stars = "";
+                for (int countStar = 0; countStar < row; countStar++)
+                {
+                    stars += "*"; 
+                }
+                Console.WriteLine(stars);
+            }
+        }
+    }
+}
